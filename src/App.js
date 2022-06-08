cd624345a26eb2a06767b2e233badb08263cd9f5
Danilo Teofilo <danigolaseu@gmail.com>
2022-06-08 17:15:02 -0300

    Instalação básica do React.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import FormLogin from './components/FormLogin';
+
+function App() {
+  return (
+    <main className='main-container'>
+      <div>
+        <p>Esquerda</p>
+      </div>
+      <div>
+       <FormLogin />
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/FormLogin.js
@@ -0,0 +1,16 @@
+import './FormLogin.css'
+
+function FormLogin () {
+    return (
+        <form className='form-login'>
+          <h3>Acesse sua conta</h3>
+          <input />
+          <input />
+          <input />
+          <p></p>
+          <button>Login</button>
+        </form>
+    )
+}
+
+export default FormLogin
