ea8fa18c48afe7967b4c060d05f0793514b3d46f
Danilo Teofilo <danigolaseu@gmail.com>
2022-06-08 17:58:05 -0300

    Estruturando o container do formulário.

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -4,11 +4,13 @@ function FormLogin () {
     return (
         <form className='form-login'>
           <h3>Acesse sua conta</h3>
-          <input />
-          <input />
-          <input />
-          <p></p>
+          <input className='input-company'/>
+          <input className='input-user'/>
+          <input className='input-password'/>
+          <a href='#' className='recover-password'>Esqueceu sua senha?</a> 
           <button>Login</button>
+          <p>Não possui uma conta?</p>
+          <a href='#' className='sign-up'>Solicitar cadastro</a>
         </form>
     )
 }
