23e7e134a39d830bceac8f54b0301c9ba7e431d8
Danilo Teofilo <danigolaseu@gmail.com>
2022-06-09 16:05:57 -0300

    Estilização dos inputs e ajustes de font-family do formulário.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import FormLogin from './components/FormLogin';
 function App() {
   return (
     <main className='main-container'>
-      <div>
-        <p>Esquerda</p>
+      <div className='left-container'>
+        <div className='logo-container'>Logo</div>
+        <div className='text-content'>
+          <h2>Tecnologia com soluções para o agronegócio</h2>
+          <p>Facilitamos a aadequação ás legislações referentes
+             a rastreabilidade vegetal, garantido a segurança
+              dos alimentos.</p>
+        </div>
+        <div>
+          Ícones: facebook, instagram e wtsapp
+        </div>
       </div>
-      <div>
+
+      <div className='right-container'>
        <FormLogin />
       </div>
     </main>
--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -4,9 +4,9 @@ function FormLogin () {
     return (
         <form className='form-login'>
           <h3 className='title-acess-account'>Acesse sua conta</h3>
-          <input className='input-company'/>
-          <input className='input-user'/>
-          <input className='input-password'/>
+          <input type='text' className='input-company' placeholder='Empresa'/>
+          <input type='text' className='input-user' placeholder='Usuário'/>
+          <input type='password' className='input-password' placeholder='Senha'/>
           <a href='#' className='recover-password'>Esqueceu sua senha?</a> 
           <button className='button-login'>Login</button>
           <p className='no-account'>Não possui uma conta?</p>
